4fa91034f1275b76f5ed4c03a5feb8b6d481adf9
szh0010 <3189922710@qq.com>
2026-02-21 15:57:54 +0800

    解决注册失败网络问题和幽灵房间问题

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // 1. 转发普通 API 请求 (登录、注册、房间列表等)
+      '/api': {
+        target: 'http://127.0.0.1:8000', // 你的本地 Django 后端地址
+        changeOrigin: true,
+        // 如果你的 Django 接口本身就带有 /api 前缀，则不需要 rewrite
+        // rewrite: (path) => path.replace(/^\/api/, '') 
+      },
+      // 2. 转发 WebSocket 请求 (对局逻辑)
+      '/ws': {
+        target: 'ws://127.0.0.1:8000',
+        ws: true,
+      }
+    }
+  }
 })
